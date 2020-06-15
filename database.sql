88e7ce9f6db0201c83ad335020193c6f850de6b3
Wyroczen <wisniewskibw@gmail.com>
2020-06-15 15:15:41 +0200

    Add files via upload
    Database creator script

---
+++ database.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 08 Cze 2020, 13:15
+-- Wersja serwera: 10.1.38-MariaDB
+-- Wersja PHP: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `minishop`
+--
+CREATE DATABASE IF NOT EXISTS `minishop` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `minishop`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `price` double NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `image`, `price`, `quantity`) VALUES
+(1, 'Sklep Mleczny', 'mleczny.jpg', 100, 1),
+(2, 'Metalowy', 'metalowy.jpg', 100, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `products_details`
+--
+
+CREATE TABLE `products_details` (
+  `id` int(11) NOT NULL,
+  `description` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `products_details`
+--
+
+INSERT INTO `products_details` (`id`, `description`) VALUES
+(1, 'Najlepszy sklep mleczny w okolicy Poznania w konkurancyjnej cenie!'),
+(2, 'Taki sklep metalowy to każdy by chciał.');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `products_details`
+--
+ALTER TABLE `products_details`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
